bed1189df0035d862db6023e2643e6200f3632be
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-07-04 23:43:17 +0200

    Initial landing page scaffolding

---
+++ apps/landing/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage: React.FC = () => {
+  return (
+    <div id="MainPage" className="flex w-full flex-col items-center">
+      <h1 className="pt-32 text-3xl font-bold tracking-tight md:text-7xl">Gardbase Landing Page</h1>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ apps/landing/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
