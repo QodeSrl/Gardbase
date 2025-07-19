1cb5e9bd3eae014f5bf6736b28a31b3b9442fd8a
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-07-19 22:40:19 +0200

    Initialize logger with zap in main.go and update dependencies in go.mod and go.sum

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -4,9 +4,16 @@ import (
 	"github.com/QodeSrl/gardbase-api/internal/handlers"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic("failed to initialize logger: " + err.Error())
+	}
+	defer logger.Sync()
+
 	r := gin.Default()
 
 	r.GET("/health", handlers.HealthCheckHandler)
