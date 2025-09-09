5d4591bd4f6bea5a01d1d790404948597feea569
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-09-09 23:54:23 +0200

    Add initial Terraform configuration files and update .gitignore for infrastructure

---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+variable "environment" {
+  description = "The environment for the deployment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+    description = "The name of the project"
+    type        = string
+    default     = "gardbase"
+}
+
+variable "region" {
+  description = "The cloud region to deploy resources in"
+  type        = string
+  default     = "eu-north-1"
+}
