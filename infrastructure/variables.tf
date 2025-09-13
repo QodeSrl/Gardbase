5fe6fa8c56db412c442005287cbadac69b734b71
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-09-13 22:49:18 +0200

    Add output variables for S3 bucket, Lambda function, and DynamoDB tables. Add instance_types to variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -15,3 +15,10 @@ variable "region" {
   type        = string
   default     = "eu-north-1"
 }
+
+variable "instance_type" {
+  description = "The type of EC2 instance to use"
+  type        = string
+  // note: nitro enclaves doesn't support all instance types (see https://docs.aws.amazon.com/enclaves/latest/user/nitro-enclave.html#nitro-enclave-reqs) - c6g.large is a good choice for optimized compute, also consider m6g.large, r6g.large, m5.xlarge
+  default     = "c6g.large"
+}
