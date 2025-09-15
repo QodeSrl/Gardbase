6b8b520379b061ea4d8fa1c46cfd0f6d2463a500
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-09-15 23:30:12 +0200

    Add Terraform configuration files for bootstrap infrastructure

--- infrastructure/bootstrap/main.tf
+++ infrastructure/bootstrap/main.tf
@@ -1,27 +1,23 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_s3_bucket" "lambdas_bucket" {
-    bucket = "gardbase-lambdas-bucket"
+    bucket = "${var.project_name}-lambdas-bucket-${var.environment}"
+    force_destroy = var.environment == "dev" ? true : false
 }
 
 resource "aws_ecr_repository" "api" {
-    name = "gardbase-api"
-}
-
-output "lambdas_bucket_name" {
-    value = aws_s3_bucket.lambdas_bucket.bucket
-    description = "Name of the S3 bucket for Lambda code"
-}
-
-output "lambda_bucket_arn" {
-    value = aws_s3_bucket.lambdas_bucket.arn
-    description = "ARN of the S3 bucket for Lambda code"
-}
-
-output "ecr_repository_url" {
-    value = aws_ecr_repository.api.repository_url
-    description = "URL of the ECR repository for the API service"
-}
-
-output "ecr_repository_name" {
-    value = aws_ecr_repository.api.name
-    description = "Name of the ECR repository for the API service"
+    name = "${var.project_name}-api"
+    force_delete = var.environment == "dev" ? true : false
 }
