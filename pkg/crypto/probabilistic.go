4b4019271da67d6997d657a56a14055d1eac6959
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-09-30 23:56:56 +0200

    Implement AES-GCM encryption and decryption functions for probabilistic encryption

--- pkg/crypto/probabilistic.go
+++ pkg/crypto/probabilistic.go
@@ -7,7 +7,11 @@
 package crypto
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
+	"errors"
+	"fmt"
 )
 
 const (
@@ -22,4 +26,96 @@ func generateRandomBytes(size int) ([]byte, error) {
 		return nil, err
 	}
 	return b, nil
+}
+
+// TODO: implement KMS integration
+func EncryptObjectProbabilistic(masterKey, pt []byte) (cipherText []byte, encryptedDEK []byte, err error) {
+	if (len(masterKey) != AESKeySize) {
+		return nil, nil, fmt.Errorf("invalid master key size: %d", len(masterKey))
+	}
+
+	// generate a random DEK
+	dek, err := generateRandomBytes(AESKeySize)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// encrypt pt with DEK
+	ct, err := aesGMCEncrypt(dek, pt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// encrypt DEK with master key
+	encryptedDEK, err = aesGMCEncrypt(masterKey, dek)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return ct, encryptedDEK, nil
+}
+
+func DecryptObjectProbabilistic(masterKey, ct, encryptedDEK []byte) (plainText []byte, err error) {
+	if (len(masterKey) != AESKeySize) {
+		return nil, fmt.Errorf("invalid master key size: %d", len(masterKey))
+	}
+	// decrypt DEK with master key
+	dek, err := aesGCMDecrypt(masterKey, encryptedDEK)
+	if err != nil {
+		return nil, err
+	}
+	// decrypt ct with DEK
+	pt, err := aesGCMDecrypt(dek, ct)
+	if err != nil {
+		return nil, err
+	}
+	return pt, nil
+}
+
+func aesGMCEncrypt(key, pt []byte) ([]byte, error) {
+	if len(key) != AESKeySize {
+		return nil, fmt.Errorf("invalid key size: %d", len(key))
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gmc, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonce, err := generateRandomBytes(GMCNonceSize)
+	if err != nil {
+		return nil, err
+	}
+	ct := gmc.Seal(nil, nonce, pt, nil)
+	// return nonce || ct
+	out := make([]byte, 0, len(nonce)+len(ct))
+	out = append(out, nonce...)
+	out = append(out, ct...)
+	return out, nil
+}
+
+func aesGCMDecrypt(key, input []byte) ([]byte, error) {
+	if len(input) < GMCNonceSize {
+		return nil, errors.New("input too short")
+	}
+	if len(key) != AESKeySize {
+		return nil, fmt.Errorf("invalid key size: %d", len(key))
+	}
+	nonce := input[:GMCNonceSize]
+	ct := input[GMCNonceSize:]
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	gmc, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	pt, err := gmc.Open(nil, nonce, ct, nil)
+	if err != nil {
+		return nil, err
+	}
+	return pt, nil
 }
