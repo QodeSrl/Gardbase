2ac249ae5feab40139f93c9a160a585e0bec35f3
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-11-26 00:50:56 +0100

    Add S3 bucket versioning, encryption, and public access block configurations

--- infrastructure/main/s3.tf
+++ infrastructure/main/s3.tf
@@ -6,3 +6,32 @@ resource "aws_s3_bucket" "uploads" {
     Name = "uploads-${var.environment}"
   }
 }
+
+// Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "uploads" {
+  bucket = aws_s3_bucket.uploads.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+// Enable server-side encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "uploads" {
+  bucket = aws_s3_bucket.uploads.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+// Block public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "uploads" {
+  bucket = aws_s3_bucket.uploads.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
