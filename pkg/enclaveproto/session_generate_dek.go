c0ebbbb1b7ca082d50b9f77678f288b6d3baad11
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-12-27 22:54:54 +0100

    Remove omitempty from JSON tags in request and response structs for stricter validation in enclaveproto

--- pkg/enclaveproto/session_generate_dek.go
+++ pkg/enclaveproto/session_generate_dek.go
@@ -2,23 +2,23 @@ package enclaveproto
 
 type SessionGenerateDEKRequest struct {
 	// Session ID, Base64-encoded
-	SessionId string `json:"session_id,omitempty"`
+	SessionId string `json:"session_id"`
 	// KMS Key ID
-	KeyId string `json:"key_id,omitempty"`
+	KeyId string `json:"key_id"`
 	// Number of DEKs to generate
-	Count int `json:"count,omitempty"`
+	Count int `json:"count"`
 }
 
 type GeneratedDEK struct {
 	// Generated DEK, Base64-encoded
-	SealedDEK string `json:"dek,omitempty"`
+	SealedDEK string `json:"dek"`
 	// Generated DEK encrypted by KMS, Base64-encoded
-	KmsEncryptedDEK string `json:"kms_encrypted_dek,omitempty"`
+	KmsEncryptedDEK string `json:"kms_encrypted_dek"`
 	// Nonce used for encryption, Base64-encoded
-	Nonce string `json:"nonce,omitempty"`
+	Nonce string `json:"nonce"`
 }
 
 type SessionGenerateDEKResponse struct {
 	// List of generated DEKs
-	DEKs []GeneratedDEK `json:"deks,omitempty"`
+	DEKs []GeneratedDEK `json:"deks"`
 }
--- pkg/enclaveproto/session_init.go
+++ pkg/enclaveproto/session_init.go
@@ -2,18 +2,18 @@ package enclaveproto
 
 type SessionInitRequest struct {
 	// Client's ephemeral public key
-	ClientEphemeralPublicKey string `json:"client_ephemeral_public_key,omitempty"`
+	ClientEphemeralPublicKey string `json:"client_ephemeral_public_key"`
 	// Session nonce, Base64-encoded
 	Nonce string `json:"nonce"`
 }
 
 type SessionInitResponse struct {
 	// Session ID, Base64-encoded
-	SessionId string `json:"session_id,omitempty"`
+	SessionId string `json:"session_id"`
 	// Enclave's ephemeral public key, Base64-encoded
-	EnclaveEphemeralPublicKey string `json:"enclave_ephemeral_public_key,omitempty"`
+	EnclaveEphemeralPublicKey string `json:"enclave_ephemeral_public_key"`
 	// Attestation document, Base64-encoded
 	Attestation string `json:"attestation"`
 	// Session expiration time, RFC3339 format
-	ExpiresAt string `json:"expires_at,omitempty"`
+	ExpiresAt string `json:"expires_at"`
 }
