f53e6bbe8be6b535288cb3a661bf636fcdfd6754
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2025-12-29 00:38:39 +0100

    Fix decryption handling enclave-side by using openssl to open kms ciphertext

--- apps/enclave-service/internal/handlers/decrypt.go
+++ apps/enclave-service/internal/handlers/decrypt.go
@@ -1,121 +1,69 @@
 package handlers
 
 import (
+	"crypto/rand"
 	"crypto/rsa"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
 
 	"github.com/QodeSrl/gardbase/pkg/enclaveproto"
+	"golang.org/x/crypto/nacl/box"
 
 	"github.com/QodeSrl/gardbase/apps/enclave-service/internal/utils"
-	"github.com/hf/nsm"
 )
 
 func HandleDecrypt(encoder *json.Encoder, payload json.RawMessage, nsmPrivKey *rsa.PrivateKey) {
-	// var req enclaveproto.DecryptRequest
-	// if err := json.Unmarshal(payload, &req); err != nil {
-	// 	utils.SendError(encoder, fmt.Sprintf("Invalid decrypt request: %v", err))
-	// 	return
-	// }
+	var req enclaveproto.DecryptRequest
+	if err := json.Unmarshal(payload, &req); err != nil {
+		utils.SendError(encoder, fmt.Sprintf("Invalid decrypt request: %v", err))
+		return
+	}
 
-	// clientPubKeyBytes, err := base64.StdEncoding.DecodeString(req.ClientEphemeralPublicKey)
-	// if err != nil || len(clientPubKeyBytes) != 32 {
-	// 	utils.SendError(encoder, "Invalid client ephemeral key")
-	// 	return
-	// }
-	// var clientPubKey [32]byte
-	// copy(clientPubKey[:], clientPubKeyBytes)
+	clientPubKeyBytes, err := base64.StdEncoding.DecodeString(req.ClientEphemeralPublicKey)
+	if err != nil || len(clientPubKeyBytes) != 32 {
+		utils.SendError(encoder, "Invalid client ephemeral key")
+		return
+	}
+	var clientPubKey [32]byte
+	copy(clientPubKey[:], clientPubKeyBytes)
 
-	// ciphertext, err := base64.StdEncoding.DecodeString(req.Ciphertext)
-	// if err != nil {
-	// 	utils.SendError(encoder, fmt.Sprintf("Invalid ciphertext encoding: %v", err))
-	// 	return
-	// }
+	ciphertext, err := base64.StdEncoding.DecodeString(req.Ciphertext)
+	if err != nil {
+		utils.SendError(encoder, fmt.Sprintf("Invalid ciphertext encoding: %v", err))
+		return
+	}
 
-	// // generate attestation document for KMS, based on NSM public key and (request's) nonce
-	// var attestationDoc []byte
-	// if nsmSession != nil {
-	// 	attestationReq := request.Attestation{
-	// 		PublicKey: pubKeyBytes,
-	// 	}
-	// 	if req.Nonce != "" {
-	// 		nonce, err := base64.StdEncoding.DecodeString(req.Nonce)
-	// 		if err != nil {
-	// 			utils.SendError(encoder, fmt.Sprintf("Invalid nonce encoding: %v", err))
-	// 			return
-	// 		}
-	// 		attestationReq.Nonce = nonce
-	// 	}
-	// 	attestationRes, err := nsmSession.Send(&attestationReq)
-	// 	if err != nil {
-	// 		utils.SendError(encoder, fmt.Sprintf("Failed to get attestation document: %v", err))
-	// 		return
-	// 	}
-	// 	if attestationRes.Attestation != nil && attestationRes.Attestation.Document != nil {
-	// 		attestationDoc = attestationRes.Attestation.Document
-	// 	}
-	// }
+	// decrypt the ciphertext for recipient using NSM private key
+	decryptedOutput, err := utils.DecryptWithOpenSSL(ciphertext, nsmPrivKey)
+	if err != nil {
+		utils.SendError(encoder, fmt.Sprintf("Failed to decrypt ciphertext for recipient: %v", err))
+		return
+	}
 
-	// // call kms to decrypt
-	// ctx := context.Background()
-	// input := &kms.DecryptInput{
-	// 	CiphertextBlob: ciphertext,
-	// 	KeyId:          &req.KeyID,
-	// }
+	// encrypt the decrypted DEK using NaCl box with client's ephemeral public key
+	enclavePubKey, enclavePrivKey, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		utils.SendError(encoder, fmt.Sprintf("Failed to generate enclave key pair: %v", err))
+		return
+	}
+	var nonce [24]byte
+	if _, err := rand.Read(nonce[:]); err != nil {
+		utils.SendError(encoder, "Failed to generate nonce")
+		return
+	}
+	ciphertextBox := box.Seal(nonce[:], decryptedOutput, &nonce, &clientPubKey, enclavePrivKey)
 
-	// // include attestation document if available
-	// if attestationDoc != nil {
-	// 	input.Recipient = &kmsTypes.RecipientInfo{
-	// 		AttestationDocument:    attestationDoc,
-	// 		KeyEncryptionAlgorithm: "RSAES_OAEP_SHA_256",
-	// 	}
-	// }
+	resp := enclaveproto.Response[enclaveproto.DecryptResponse]{
+		Success: true,
+		Message: "Decryption successful",
+		Data: enclaveproto.DecryptResponse{
+			EnclavePubKey: base64.StdEncoding.EncodeToString(enclavePubKey[:]),
+			Ciphertext:    base64.StdEncoding.EncodeToString(ciphertextBox),
+			Nonce:         base64.StdEncoding.EncodeToString(nonce[:]),
+			RequestNonce:  req.Nonce,
+		},
+	}
 
-	// output, err := kmsClient.Decrypt(ctx, input)
-	// if err != nil {
-	// 	utils.SendError(encoder, fmt.Sprintf("KMS decrypt failed: %v", err))
-	// 	return
-	// }
-	// if len(output.CiphertextForRecipient) == 0 {
-	// 	utils.SendError(encoder, "KMS did not return ciphertext for recipient")
-	// 	return
-	// }
-
-	// // decrypt the ciphertext for recipient using NSM private key
-	// decryptedOutput, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, nsmPrivKey, output.CiphertextForRecipient, nil)
-	// if err != nil {
-	// 	utils.SendError(encoder, fmt.Sprintf("Failed to decrypt ciphertext for recipient: %v", err))
-	// 	return
-	// }
-
-	// // encrypt the decrypted DEK using NaCl box with client's ephemeral public key
-	// enclavePubKey, enclavePrivKey, err := box.GenerateKey(rand.Reader)
-	// if err != nil {
-	// 	utils.SendError(encoder, fmt.Sprintf("Failed to generate enclave key pair: %v", err))
-	// 	return
-	// }
-	// var nonce [24]byte
-	// if _, err := rand.Read(nonce[:]); err != nil {
-	// 	utils.SendError(encoder, "Failed to generate nonce")
-	// 	return
-	// }
-	// ciphertextBox := box.Seal(nonce[:], decryptedOutput, &nonce, &clientPubKey, enclavePrivKey)
-
-	// resp := enclaveproto.Response[enclaveproto.DecryptResponse]{
-	// 	Success: true,
-	// 	Message: "Decryption successful",
-	// 	Data: enclaveproto.DecryptResponse{
-	// 		EnclavePubKey: base64.StdEncoding.EncodeToString(enclavePubKey[:]),
-	// 		Ciphertext:    base64.StdEncoding.EncodeToString(ciphertextBox),
-	// 		Nonce:         base64.StdEncoding.EncodeToString(nonce[:]),
-	// 		RequestNonce:  req.Nonce,
-	// 		Attestation:   base64.StdEncoding.EncodeToString(attestationDoc),
-	// 	},
-	// }
-
-	// utils.SendResponse(encoder, resp)
-
-	utils.SendResponse(encoder, enclaveproto.Response[any]{
-		Success: false,
-		Error:   "TODO",
-	})
+	utils.SendResponse(encoder, resp)
 }
