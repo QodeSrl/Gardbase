7e57f588d625569923ab9a70976f5d0e7d4ccfee
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2026-02-02 22:19:53 +0100

    Add new request and response types for scanning objects; enhance CreateObjectRequest with BlobSize

--- pkg/api/objects/requests.go
+++ pkg/api/objects/requests.go
@@ -7,9 +7,16 @@ type GetTableHashRequest struct {
 }
 
 type CreateObjectRequest struct {
+	BlobSize           int64             `json:"blob_size" binding:"required"`
 	KMSEncryptedDEK    string            `json:"encrypted_dek" binding:"required"`
 	MasterEncryptedDEK string            `json:"master_encrypted_dek" binding:"required"`
 	DEKNonce           string            `json:"dek_nonce" binding:"required"`
 	Indexes            map[string]string `json:"indexes,omitempty"`
 	Sensitivity        string            `json:"sensitivity,omitempty" binding:"omitempty,oneof=low medium high"`
+}
+
+type ScanRequest struct {
+	TableHash string  `json:"table_hash" binding:"required"`
+	Limit     int     `json:"limit,omitempty"`
+	NextToken *string `json:"next_token,omitempty"`
 }
